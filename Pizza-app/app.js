47a347a88179bcab12c91b7029023bdb0974334a
George Uzoh <ugochukwuuzoh@gmail.com>
2023-09-17 18:05:21 +0300

    Add correct files

--- Pizza-app/app.js
+++ Pizza-app/app.js
@@ -1,17 +1,21 @@
 // Selects all inputs to listen to.
 const formItems = document.querySelector('form');
-
 const customer = document.querySelector('#name');
 const address = document.querySelector('#address');
 const sizes = document.querySelectorAll('input[name=size]');
 const toppings = document.querySelectorAll('input[type=checkbox]');
 const deliveryMethod = document.querySelector('#delivery');
 const priceDisplay = document.querySelector('#price');
+const orderButton = document.querySelector('button');
+const customerInfo = document.querySelector('#customer');
+const pizzaSize = document.querySelector('#size');
+const customerAddress = document.querySelector('#customer-address');
+const finalToppings = document.querySelector('#toppings');
+const delivery = document.querySelector('#delivery-option');
+const totalPrice = document.querySelector('#total');
 
 // Function that computes the total price based on inputs selected.
 const getTotalPizzaPrice = () => {
-    const customerName = customer.value;
-    
     // Initial base and extra fees.
     let sizeId = '';
     let toppingsList = [];
@@ -23,10 +27,10 @@ const getTotalPizzaPrice = () => {
         if (size.checked) sizeId = size.id;
     }
 
-    if (sizeId === 'two') price += 7.5;
-    else if (sizeId === 'four') price += 10.5;
-    else if (sizeId === 'six') price += 12.5;
-    else if (sizeId === 'eight') price += 15.5;
+    if (sizeId === 'small') price += 7.5;
+    else if (sizeId === 'medium') price += 10.5;
+    else if (sizeId === 'large') price += 12.5;
+    else if (sizeId === 'extra-large') price += 15.5;
 
     // Gets selected topping(s) from the toppings list.
     toppings.forEach(topping => {
@@ -37,9 +41,48 @@ const getTotalPizzaPrice = () => {
     // Gets selected delivery method.
     if (deliveryOption === 'home') price += 5;
 
-    priceDisplay.textContent = price.toFixed(2);
+    // Displays total price to the customer in real time.
+    priceDisplay.textContent = price;
+    return price;
 
 }
 
 // Adds event listener to the form.
 formItems.addEventListener('change', getTotalPizzaPrice);
+
+// Function to print the summary of the customer's order.
+const getOrderSummary = () => {
+    // Global variables in function scope to access their values.
+    const customerName = customer.value;
+    const addressInfo = address.value;
+    let sizeId = '';
+    const allToppings = [];
+    const deliveryType = deliveryMethod.options[deliveryMethod.selectedIndex].value;
+    const summary = document.querySelector('.summary');
+    
+    customerInfo.textContent = `Customer: ${customerName}`;
+    customerAddress.textContent = `Address: ${addressInfo}`;
+
+    for (const size of sizes) {
+        if (size.checked) sizeId = size.id;
+    }
+    pizzaSize.textContent = `Size: ${sizeId}`;
+
+    // All selected toppings.
+    for (const topping of toppings) {
+        if (topping.checked) allToppings.push(topping.id);
+    }
+    finalToppings.textContent = `Toppings: ${allToppings.join(', ')}`;
+
+    // Delivery type.
+    delivery.textContent = `Delivery Option: ${deliveryType}`;
+
+    // Price.
+    totalPrice.textContent = `Price: ${getTotalPizzaPrice()} €`;
+
+    // Removes the hidden class applied to the summary container and reveals the customer's order summary.
+    summary.classList.remove('hidden');
+}
+
+// Add event listener to the button.
+orderButton.addEventListener('click', getOrderSummary);
