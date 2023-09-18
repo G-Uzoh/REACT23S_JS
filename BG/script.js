9d0e549c1d5faf39bafd3af3feed5ad759bdae03
George Uzoh <ugochukwuuzoh@gmail.com>
2023-09-18 14:23:40 +0300

    Update generator function

--- BG/script.js
+++ BG/script.js
@@ -3,8 +3,6 @@ const color1 = document.querySelector('#color1');
 const color2 = document.querySelector('#color2');
 const directions = document.querySelectorAll('input[type=radio]');
 const colorCodes = document.querySelector('#code');
-const body = document.querySelector('body');
-console.log(body);
 
 const bgGenerator = () => {
     let selectedValue;
@@ -12,8 +10,15 @@ const bgGenerator = () => {
     for (const direction of directions) {
         direction.checked ? selectedValue = direction.value : '';
     }
-    colorCodes.textContent = `linear-gradient(${selectedValue}, ${color1.value}, ${color2.value});`;
-    // body.bgColor = 
+
+    let gradient = `linear-gradient(${selectedValue}, ${color1.value}, ${color2.value})`;
+    
+    // Apply style to the body
+    document.body.style.backgroundImage = gradient;
+
+    // Display the color codes.
+    colorCodes.textContent = gradient;
 }
 
+// Add event listener to the form
 form.addEventListener('change', bgGenerator);
