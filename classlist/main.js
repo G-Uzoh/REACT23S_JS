40a59c83b05e12b326fc9ac8a5f09327d16cfbf0
George Uzoh <ugochukwuuzoh@gmail.com>
2023-09-19 12:58:46 +0300

    Add mobile responsive design

--- classlist/main.js
+++ classlist/main.js
@@ -1,7 +1,11 @@
 const toTopButton = document.querySelector('#back-to-top');
+const mobileMenuBtn = document.querySelector('.mobile');
+const navMenu = document.querySelector('nav ul');
 
+// Scroll event on window.
 window.onscroll = function() {scrollFunction()};
 
+// Code from w3schools for scrolling (for two different browsers).
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     toTopButton.style.display = "block";
@@ -15,4 +19,9 @@ const backToTop = () => {
     document.documentElement.scrollTop = 0;
 }
 
+const mobileMenu = () => {
+    navMenu.classList.contains('responsive') ? navMenu.classList.remove('responsive') : navMenu.classList.add('responsive');
+}
+
 toTopButton.addEventListener('click', backToTop);
+mobileMenuBtn.addEventListener('click', mobileMenu);
