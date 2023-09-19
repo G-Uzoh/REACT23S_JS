10d16f332cce4a9ffd141a825989d94fb7a22122
George Uzoh <ugochukwuuzoh@gmail.com>
2023-09-19 13:19:13 +0300

    Add functionality to mobile menu

--- classlist/main.js
+++ classlist/main.js
@@ -1,17 +1,17 @@
 const toTopButton = document.querySelector('#back-to-top');
 const mobileMenuBtn = document.querySelector('.mobile');
 const navMenu = document.querySelector('nav ul');
+const menuItems = document.querySelectorAll('nav ul li a');
+const header = document.querySelector('header');
 
 // Scroll event on window.
 window.onscroll = function() {scrollFunction()};
 
 // Code from w3schools for scrolling (for two different browsers).
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    toTopButton.style.display = "block";
-  } else {
-    toTopButton.style.display = "none";
-  }
+  (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) ? toTopButton.style.display = "block" : toTopButton.style.display = "none";
+
+  (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) ? header.classList.add('bg') : header.classList.remove('bg');
 }
 
 const backToTop = () => {
@@ -20,6 +20,9 @@ const backToTop = () => {
 }
 
 const mobileMenu = () => {
+    for (item of menuItems) {
+        item.addEventListener('click', mobileMenu);
+    }
     navMenu.classList.contains('responsive') ? navMenu.classList.remove('responsive') : navMenu.classList.add('responsive');
 }
 
