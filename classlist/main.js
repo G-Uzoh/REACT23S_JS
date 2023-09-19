7f08af41f99584aa1c8acbaafd99387ded01aa29
George Uzoh <ugochukwuuzoh@gmail.com>
2023-09-19 11:19:21 +0300

    Fix button position

--- classlist/main.js
+++ classlist/main.js
@@ -1,5 +1,15 @@
 const toTopButton = document.querySelector('#back-to-top');
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    toTopButton.style.display = "block";
+  } else {
+    toTopButton.style.display = "none";
+  }
+}
+
 const backToTop = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
